6105313a8dae07c0cc982f9af50cb51d584ce5a2
Carlos Vieira <cvieirasp@gmail.com>
2020-02-15 11:17:52 -0300

    Adição de Função Ajax para Busca de CEP

--- src/Store.App/wwwroot/js/site.js
+++ src/Store.App/wwwroot/js/site.js
@@ -39,3 +39,57 @@
         }
     });
 }
+
+function GetCEP() {
+    $(document).ready(function () {
+
+        function clearFormCEP() {
+            $("#Address_PublicPlace").val("");
+            $("#Address_District").val("");
+            $("#Address_City").val("");
+            $("#Address_State").val("");
+        }
+
+        function setReadOnly(readonly) {
+            $('#Address_PublicPlace').attr('readonly', readonly);
+            $('#Address_District').attr('readonly', readonly);
+            $('#Address_City').attr('readonly', readonly);
+            $('#Address_State').attr('readonly', readonly);
+        }
+
+        $("#Address_CEP").blur(function () {
+            var cep = $(this).val().replace(/\D/g, '');
+
+            if (cep != "") {
+                var validateCEP = /^[0-9]{8}$/;
+
+                if (validateCEP.test(cep)) {
+                    setReadOnly(true);
+                    $("#Address_PublicPlace").val("...");
+                    $("#Address_District").val("...");
+                    $("#Address_City").val("...");
+                    $("#Address_State").val("...");
+
+                    $.getJSON("https://viacep.com.br/ws/" + cep + "/json/?callback=?", function (data) {
+                        if (!("erro" in data)) {
+                            $("#Address_PublicPlace").val(data.logradouro);
+                            $("#Address_District").val(data.bairro);
+                            $("#Address_City").val(data.localidade);
+                            $("#Address_State").val(data.uf);
+                            setReadOnly(false);
+                        } else {
+                            clearFormCEP();
+                            setReadOnly(false);
+                            alert("CEP não encontrado.");
+                        }
+                    });
+                } else {
+                    clearFormCEP();
+                    alert("CEP com formato inválido.");
+                }
+            } else {
+                clearFormCEP();
+            }
+        });
+    });
+}
